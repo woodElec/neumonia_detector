c9788f24496ad5e2a9b624880469aba51f50672f
Arturo <arduque92@gmail.com>
2022-08-13 13:04:55 -0500

    Eliminar una linea de un archivo

--- detector_neumonia.py
+++ detector_neumonia.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -13,29 +12,34 @@ import tkcap
 import img2pdf
 import numpy as np
 import time
+
 # -----------------------------
 #   OWN SCRIPTS
 import read_img
 import integrator
 
 
-class App():
+class App:
     def __init__(self):
         self.root = Tk()
         self.root.title("Herramienta para la detección rápida de neumonía")
 
         #   BOLD FONT
-        fonti = font.Font(weight='bold')
+        fonti = font.Font(weight="bold")
 
-        self.root.geometry("815x560")        
-        self.root.resizable(0,0)
+        self.root.geometry("815x560")
+        self.root.resizable(0, 0)
 
         #   LABELS
         self.lab1 = ttk.Label(self.root, text="Imagen Radiográfica", font=fonti)
         self.lab2 = ttk.Label(self.root, text="Imagen con Heatmap", font=fonti)
         self.lab3 = ttk.Label(self.root, text="Resultado:", font=fonti)
         self.lab4 = ttk.Label(self.root, text="Cédula Paciente:", font=fonti)
-        self.lab5 = ttk.Label(self.root, text="SOFTWARE PARA EL APOYO AL DIAGNÓSTICO MÉDICO DE NEUMONÍA", font=fonti)
+        self.lab5 = ttk.Label(
+            self.root,
+            text="SOFTWARE PARA EL APOYO AL DIAGNÓSTICO MÉDICO DE NEUMONÍA",
+            font=fonti,
+        )
         self.lab6 = ttk.Label(self.root, text="Probabilidad:", font=fonti)
 
         #   TWO STRING VARIABLES TO CONTAIN ID AND RESULT
@@ -55,15 +59,21 @@ class App():
         self.text3 = Text(self.root)
 
         #   BUTTONS
-        self.button1 = ttk.Button(self.root, text="Predecir", state='disabled', command=self.run_model) 
-        self.button2 = ttk.Button(self.root, text="Cargar Imagen", command=self.load_img_file)
+        self.button1 = ttk.Button(
+            self.root, text="Predecir", state="disabled", command=self.run_model
+        )
+        self.button2 = ttk.Button(
+            self.root, text="Cargar Imagen", command=self.load_img_file
+        )
         self.button3 = ttk.Button(self.root, text="Borrar", command=self.delete)
         self.button4 = ttk.Button(self.root, text="PDF", command=self.create_pdf)
-        self.button6 = ttk.Button(self.root, text="Guardar", command=self.save_results_csv)
+        self.button6 = ttk.Button(
+            self.root, text="Guardar", command=self.save_results_csv
+        )
 
-        #   WIDGETS POSITIONS   
+        #   WIDGETS POSITIONS
         self.lab1.place(x=110, y=65)
-        self.lab2.place(x=545, y=65)        
+        self.lab2.place(x=545, y=65)
         self.lab3.place(x=500, y=350)
         self.lab4.place(x=65, y=350)
         self.lab5.place(x=122, y=25)
@@ -83,65 +93,75 @@ class App():
         self.text1.focus_set()
 
         #  se reconoce como un elemento de la clase
-        self.array = None 
+        self.array = None
 
         #   NUMERO DE IDENTIFICACIÓN PARA GENERAR PDF
         self.reportID = 0
 
         #   RUN LOOP
-        self.root.mainloop()  
-        
+        self.root.mainloop()
+
     #   METHODS
     def load_img_file(self):
-        filepath = filedialog.askopenfilename(initialdir="/", title="Select image", filetypes=(('DICOM','*.dcm'),('JPEG','*.jpeg'),('jpg files','*.jpg'),('png files','*.png')))
-        if filepath:            
+        filepath = filedialog.askopenfilename(
+            initialdir="/",
+            title="Select image",
+            filetypes=(
+                ("DICOM", "*.dcm"),
+                ("JPEG", "*.jpeg"),
+                ("jpg files", "*.jpg"),
+                ("png files", "*.png"),
+            ),
+        )
+        if filepath:
             self.array, img2show = read_img.read_dicom_file(filepath)
-            self.img1 = img2show.resize((250,250), Image.ANTIALIAS)
+            self.img1 = img2show.resize((250, 250), Image.ANTIALIAS)
             self.img1 = ImageTk.PhotoImage(self.img1)
             self.text_img1.image_create(END, image=self.img1)
-            self.button1['state'] = 'enabled'
+            self.button1["state"] = "enabled"
 
     def run_model(self):
-        self.label, self.proba, self.heatmap = integrator.predict(self.array) 
+        self.label, self.proba, self.heatmap = integrator.predict(self.array)
         self.img2 = Image.fromarray(self.heatmap)
-        self.img2 = self.img2.resize((250,250), Image.ANTIALIAS)
+        self.img2 = self.img2.resize((250, 250), Image.ANTIALIAS)
         self.img2 = ImageTk.PhotoImage(self.img2)
-        print('OK')
+        print("OK")
         self.text_img2.image_create(END, image=self.img2)
         self.text2.insert(END, self.label)
-        self.text3.insert(END, '{:.2f}'.format(self.proba)+'%')        
-    
+        self.text3.insert(END, "{:.2f}".format(self.proba) + "%")
+
     def save_results_csv(self):
-        with open('historial.csv', 'a') as csvfile:
-            w=csv.writer(csvfile, delimiter='-')
-            w.writerow([self.text1.get(), self.label, '{:.2f}'.format(self.proba)+'%'])
-            showinfo(title='Guardar', message='Los datos se guardaron con éxito.')
+        with open("historial.csv", "a") as csvfile:
+            w = csv.writer(csvfile, delimiter="-")
+            w.writerow(
+                [self.text1.get(), self.label, "{:.2f}".format(self.proba) + "%"]
+            )
+            showinfo(title="Guardar", message="Los datos se guardaron con éxito.")
 
     def create_pdf(self):
         cap = tkcap.CAP(self.root)
-        ID = 'Reporte' + str(self.reportID)+'.jpg'
+        ID = "Reporte" + str(self.reportID) + ".jpg"
         img = cap.capture(ID)
         img = Image.open(ID)
-        img = img.convert('RGB')
-        pdf_path = r'Reporte' + str(self.reportID)+'.pdf'
+        img = img.convert("RGB")
+        pdf_path = r"Reporte" + str(self.reportID) + ".pdf"
         img.save(pdf_path)
         self.reportID += 1
-        showinfo(title='PDF', message='El PDF fue generado con éxito.')
+        showinfo(title="PDF", message="El PDF fue generado con éxito.")
 
     def delete(self):
-        answer = askokcancel(title='Confirmación', message='Se borrarán todos los datos.', icon=WARNING)
+        answer = askokcancel(
+            title="Confirmación", message="Se borrarán todos los datos.", icon=WARNING
+        )
         if answer:
-            self.text1.delete(0, 'end')
-            self.text2.delete(1.0, 'end')
-            self.text3.delete(1.0, 'end')
-            self.text_img1.delete(self.img1, 'end')
-            self.text_img2.delete(self.img2, 'end')
-            showinfo(title='Borrar', message='Los datos se borraron con éxito')           
+            self.text1.delete(0, "end")
+            self.text2.delete(1.0, "end")
+            self.text3.delete(1.0, "end")
+            self.text_img1.delete(self.img1, "end")
+            self.text_img2.delete(self.img2, "end")
+            showinfo(title="Borrar", message="Los datos se borraron con éxito")
+
 
 def main():
     my_app = App()
     return 0
-
-if __name__ == '__main__':
-    main()
-
